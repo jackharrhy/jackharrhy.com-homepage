fd25fcf33ba8350298e2b40ba2688aa733f57dbc
Jack Arthur Harrhy <jack@jackharrhy.com>
2015-11-15 20:31:22 -0330

    divsuggaaar

--- static/index.js
+++ static/index.js
@@ -1,7 +1,18 @@
+var frame = -1;
+
+// DIV SUGAR TITLE
+var scn = new DivSugar.Scene().setSize(400,250).setImage('#f9f9f9').appendTo(document.getElementById('header')),
+	rotNode = new DivSugar.Node().setPosition(200,125,0).appendTo(scn),
+	textNode = new DivSugar.Node().setSize(400,125).setPosition(-200,-75,0).appendTo(rotNode);
+
+textNode.div.innerHTML = '<h1>jackharrhy</h1><h2>i make stuff</h2>';
+
+var task = new DivSugar.Task().appendTo(DivSugar.rootTask);
+task.onUpdate = function() { rotNode.rotate(0, this.deltaTime * 0.005, this.deltaTime * 0.0025); };
+
 // CANVAS
 var canvas = document.getElementById('canvas'),
 	c = canvas.getContext('2d'),
-	frame = -1,
 	cursorX;
 
 document.onmousemove = function(e) {
